885cf63ce67d19d4cdbcf5aeb9c03c8db493350b
Pavel Kozhemjachenko <pkozhem@gmail.com>
2025-08-03 21:17:22 +0300

    Move table base into sep file.

--- src/fastapi_orm_ext/table/__init__.py
+++ src/fastapi_orm_ext/table/__init__.py
@@ -1,23 +1,3 @@
-from fastapi_orm_ext.table.mixins import (
-    NameConventionMixin,
-    TableNameMixin,
-    TimestampsMixin,
-    UUIDPrimaryKeyMixin,
-)
+from fastapi_orm_ext.table._table import TableBase
 
 __all__ = ("TableBase",)
-
-
-class TableBase(
-    NameConventionMixin,
-    TableNameMixin,
-    TimestampsMixin,
-    UUIDPrimaryKeyMixin,
-):
-    """Base class for SQLAlchemy tables.
-
-    This class combines several mixins to provide a consistent table structure.
-    It includes automatic table naming, UUID primary key, and timestamp fields.
-    """
-
-    __abstract__: bool = True
